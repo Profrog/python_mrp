f0ea0e1ab4d860e9d7c737b8aad378c80985840a
profrog <ache159@naver.com>
2022-07-06 12:01:28 +0900

    Update map_mrp.py

--- map_mrp.py
+++ map_mrp.py
@@ -24,11 +24,15 @@ bound = 1 #0일때는 모두 같은색, 1일때는 b_time 간격으로 다른색
 file_name = 'stop'
 
 
+lat_m = 180
+lon_m = 150
+
+
 def lat(t):
- return float(t[:2]) + float(t[2:])/60
+ return (float(t[:2]) + float(t[2:])/60)
 
 def lon(g):
- return float(g[:3]) + float(g[3:])/60
+ return (float(g[:3]) + float(g[3:])/60)
  
 data2 = open(file_name + '.txt', 'r')
 #data3 = open('output.txt', 'a')
@@ -43,17 +47,23 @@ test1 = np.zeros((l_size,l_size))
 
 
 def point_mark(x1,x2,y1,y2,data):    
- for x0 in range(x1,x2 +1,1):
-   for y0 in range(y1,y2 +1,1):
-     test1[x0,y0] = int(data) #after using other way  
+ for x0 in range(x1,x2+1,1):
+   for y0 in range(y1,y2+1,1):
+    
+    x00 = x0 if x0 < l_size else 0
+    y00 = y0 if y0 < l_size else 0
+    test1[x00,y00] = int(data)
+     
+     
+      
 
 
 while string0:
  try: 
   data_list0 = string0.split(forsplit)
   time_a = float(data_list0[0])
-  x0 = str(float(data_list0[1]))[7:10]
-  y0 = str(float(data_list0[3]) -100)[7:10]
+  x0 = str(float(lat(data_list0[1])))[7:10]
+  y0 = str(float(lon(data_list0[3])) -100)[7:10]
   x1 = int(x0)
   y1 = int(y0)
   
@@ -69,7 +79,7 @@ while string0:
   string0 = data2.readline()
    
  except:
-  print("error " + string0) 
+  print("error " + str(x1) + " " + str(y1)) 
 
 
 #cmap0 = plt.get_cmap('Greys')
