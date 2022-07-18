4a14118cdedfcf11674b9b8bbbd91075697807d1
profrog <ache159@naver.com>
2022-07-18 12:47:32 +0900

    Update test_mrp.py

--- test_mrp.py
+++ test_mrp.py
@@ -6,14 +6,30 @@ import argparse
 import cv2
 import sys
 import time
+import codecs
+from datetime import datetime
+now = datetime.now()
+now_0 = str(now.hour) + ":" + str(now.minute) + ":" + str(now.second)
+
+
+raw_0 = 'data1' + now_0 + ".txt" 
+raw_1 = 'data2' + now_0 + ".txt"
 
 start0 = time.time()
 end0 = time.time()
 os.system("sudo chmod a+rw /dev/ttyUSB0")
 global ser
 ser = serial.Serial('/dev/ttyUSB0', 115200)
-data0 =  open('data1.txt', 'a')
-data =  open('data2.txt', 'a')
+data0 =  open(raw_0, 'a')
+data =  open(raw_1, 'a')
+
+
+def lat(t):
+ return (float(t[:2]) + float(t[2:])/60)
+
+def lon(g):
+ return (float(g[:3]) + float(g[3:])/60)
+
 
 forsplit = '	'
 
@@ -23,12 +39,15 @@ while end0 - start0 <= 600:
  string0 = ''
  end0 = time.time()
  line = ser.readline()
+ #line = str(line, errors='ignore')
+ 
+ print(line)
  data0.write(line.decode("utf-8"))
  data_list = line.decode("utf-8").split(',')
  #print(data_list[0])
  
  if data_list[0] == '$GNGLL':
-  string0 = str(end0-start0) + forsplit + data_list[1] + forsplit + data_list[3] + "\n"
+  string0 = str(end0-start0) + forsplit + str(lat(data_list[1])) + forsplit + str(lon(data_list[3])) + "\n"
   print(str(end0-start0) + ": " + string0)
   #print(i)
   data.write(string0)
